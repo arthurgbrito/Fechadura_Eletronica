11ad133ea0015b76ac80684e4bb676dc95433516
Arthur Gessler de Brito <arthurgesslerdebrito@gmail.com>
2025-09-11 22:26:33 -0300

    criando a api para verificar solicitações

---
+++ APIs/solicitacoes.php
@@ -0,0 +1,22 @@
+<?php 
+
+    include_once('../database/conexao.php');
+
+    $sql = "SELECT s.id, s.usuario_id, u.Username, s.status_cadastro, a.log FROM solicitacoes s JOIN usuarios u ON s.usuario_id = u.id WHERE s.status_cadastro = 'pendente' ORDER BY s.id ASC LIMIT 1";
+
+    $result = mysqli_query($conn, $sql);
+    
+    $response = [];
+
+    if (mysqli_num_rows($result) > 0){
+        $row = mysqli_fetch_assoc($result);
+        $response = $row;
+    } else {
+        $response = ["mensagem" => "nenhuma_solicitacao"];
+    }
+
+    header('Content-Type: application/json');
+
+    echo json_encode($response);
+
+?>
