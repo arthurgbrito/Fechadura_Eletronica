ebc34474368e6bda2d4927cdaa23c727340316ab
Arthur Gessler de Brito <arthurgesslerdebrito@gmail.com>
2025-09-14 17:53:21 -0300

    melhorias na API que atualiza o banco de dados em caso de erros

--- APIs/atualizaDB.php
+++ APIs/atualizaDB.php
@@ -15,27 +15,30 @@
     if (mysqli_num_rows($result) > 0){ 
 
         // Completa as informações na tabela de solicitações, repassando o erro lá
-        $sql = "UPDATE solicitacoes SET status_cadastro = 'erro', cracha = '0' WHERE id = '$id_solicitacao'";
+        $sql = "SELECT usuario_id FROM solicitacoes WHERE id = '$id_solicitacao' LIMIT 1";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        $usuario_id = $row['usuario_id'];
+
+        $sql = "DELETE FROM solicitacoes WHERE id = '$id_solicitacao'";
         mysqli_query($conn, $sql);
 
-
         // Apaga as informações do usuário que tentou se cadastrar
-        $sql = "DELETE u FROM usuarios u JOIN solicitacoes s ON u.id = s.usuario_id WHERE s.id = '$id_solicitacao'";
+        $sql = "DELETE FROM usuarios WHERE id = '$usuario_id'";
         mysqli_query($conn, $sql);
 
         echo json_encode(["status" => "erro"]);
-        exit;
+        
+    } else {
+        
+        // Se não existir esse crachá no banco, aí sim será feito o UPDATE dele e do ID do usuário no banco de solicitações
+        $sql = "UPDATE solicitacoes SET status_cadastro = 'concluido', cracha = '$cracha' WHERE id = '$id_solicitacao'";
+        mysqli_query($conn, $sql);
+
+        //Aqui será feito o UPDATE no banco de usuários, com o crachá a partir do id retornado acima
+        $sql = "UPDATE usuarios u JOIN solicitacoes s ON u.id = s.usuario_id SET u.cracha = '$cracha' WHERE s.id = '$id_solicitacao'";
+        mysqli_query($conn, $sql);
+
+        echo json_encode(["status" => "ok"]);
     }
-
-
-    // Se não existir esse crachá no banco, aí sim será feito o UPDATE dele e do ID do usuário no banco de solicitações
-    $sql = "UPDATE solicitacoes SET status_cadastro = 'concluido', cracha = '$cracha' WHERE id = '$id_solicitacao'";
-    mysqli_query($conn, $sql);
-
-    //Aqui será feito o UPDATE no banco de usuários, com o crachá a partir do id retornado acima
-    $sql = "UPDATE usuarios u JOIN solicitacoes s ON u.id = s.usuario_id SET u.cracha = '$cracha' WHERE s.id = '$id_solicitacao'";
-    mysqli_query($conn, $sql);
-
-    echo json_encode(["status" => "ok"]);
-
 ?>
