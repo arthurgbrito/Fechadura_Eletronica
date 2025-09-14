f9e925d1e8bc7ffd02d5859ce2d8bc2dc604e748
Arthur Gessler de Brito <arthurgesslerdebrito@gmail.com>
2025-09-14 17:52:54 -0300

    API que faz a leitura dos crachás pronta

--- APIs/leiaCartao.php
+++ APIs/leiaCartao.php
@@ -1,7 +1,25 @@
 <?php 
 
-include_once("../database/conexao.php");
+    include_once("../database/conexao.php");
 
-$sql = "SELECT usuarios"
+    $cracha = $_GET['cracha'] ?? '';
+    $response = ["autorizado" => false];
+
+    if (!empty($cracha)){
+        $sql = "SELECT Username FROM usuarios WHERE cracha = '$cracha' LIMIT 1";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0){
+            $user = mysqli_fetch_assoc($result);
+            
+            $response = [ 
+                "autorizado" => true,
+                "username" => $user['Username']
+            ];
+        }
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
 
 ?>
