80ec42e15fa3d3267d6ced2dab72736080b79a7b
Arthur Gessler de Brito <arthurgesslerdebrito@gmail.com>
2025-11-11 21:26:17 -0300

    melhoria na lógica de espera de cadastro

--- APIs/atualizaDB.php
+++ APIs/atualizaDB.php
@@ -5,13 +5,21 @@
     // Recebe os dados via POST
     $id_solicitacao = $_POST['id_solicitacao'];
     $cracha = $_POST['cracha'];
+    $flag = (bool)$_POST['flag'];
 
     // Consulta o banco de dados para verificar se já está cadastrado o novo crachá
     $sql = "SELECT id FROM usuarios WHERE cracha = '$cracha' LIMIT 1";
     $result = mysqli_query($conn, $sql);
 
     // Se já existir algum usuário com esse crachá cadastrado, retorna um JSON de erro e para imediatamente a execução do script
-    if (mysqli_num_rows($result) > 0){ 
+    
+    if ($flag){
+        $sql = "UPDATE solicitacoes SET status_cadastro = 'erro', cracha = NULL WHERE id = '$id_solicitacao'";
+        mysqli_query($conn, $sql);
+        
+        echo json_encode(["ok" => true]);
+
+    } else if (mysqli_num_rows($result) > 0){ 
 
         // Completa as informações na tabela de solicitações, repassando o erro lá
         $sql = "SELECT usuario_id FROM solicitacoes WHERE id = '$id_solicitacao' LIMIT 1";
--- scripts/cadastroScript.js
+++ scripts/cadastroScript.js
@@ -1,28 +1,35 @@
 
 let flagCadastro = 0;
+let contador = 0;
 
+// Ao carregar a página, executa o seguinte código
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Pega os elementos do DOM necessários
     const section = document.getElementById("conteudo");
     const aproxime = document.getElementById("aproximeCracha");
     const form = document.getElementById('formCadastro');
-
     const olho = document.getElementById('olho');
     const senha = document.getElementById('senha');
 
+    // Responsável por mostrar/ocultar a senha ao clicar no olho
     olho.addEventListener('click', () => {
         const senhaVisivel = senha.type ==='text';
-        senha.type = senhaVisivel ? 'password' : 'text';
+        senha.type = senhaVisivel ? 'password' : 'text'; // Inverte o tipo de caixa de texto
         
+        // Altera o ícone de olho fechado/aberto
         olho.classList.toggle('bi-eye-fill', senhaVisivel);
         olho.classList.toggle('bi-eye-slash-fill', !senhaVisivel);
     });
 
+    // Responsável por monitorar o botão de envio do formulário
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
 
         const formData = new FormData(form);
 
         try {
+            // Chama o arquivo de cadastro que irá processar os dados de cadastro
             const response = await fetch("../templates/cadastro.php", {
             method: "POST",
             body: formData
@@ -30,16 +37,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const data = await response.json(); 
 
+            // Se der certo o processo feito em cadastro.php
             if(data.ok){
+                // Verifica se os elementos existem antes de mexer com eles
                 if(section && aproxime){
-                    section.style.display = "none";
-                    aproxime.style.display = "flex";
-                    flagCadastro = 1;
-                    monitoraCadastro(data.solicitacao_id);
-                } else {
+                    section.style.display = "none"; // Esconde a seção de cadastro
+                    aproxime.style.display = "flex"; // Mostra a seção de aproximação do crachá
+                    flagCadastro = 1; // Seta a flag para iniciar o monitoramnento do cadastro
+                    monitoraCadastro(data.solicitacao_id); // Chama a função que monitora o estado do cadastro
+                } else { // Se existir algum problema ao pegar os elementos
                     alert("Erro: não achei os elementos no DOM");
                 }
-            } else {
+            } else { // Se der erro no processo feito em cadastro.php
                 alert("Erro ao cadastrar: " + (data.error || "desconhecido"));
             }
 
@@ -49,6 +58,8 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+
+// Função que monitora o estado atual de cadastro, para atualizar o site conforme o status
 async function monitoraCadastro(idSolicitacao) {
     if (flagCadastro == 1) {
         try {
@@ -57,18 +68,57 @@ async function monitoraCadastro(idSolicitacao) {
 
             const data = await resp.json();
 
-            if (data.ok) {
-                console.log(data.status_cadastro)
+            if (data.ok && contador <= 240) {
+
                 if (data.status_cadastro == 'concluido') {
                     mostrarModalSucesso();
                     setTimeout(() => window.location.href = "login.php", 3000);
                     flagCadastro = 0;
+
                 } else if (data.status_cadastro == 'pendente') {
-                    setTimeout(() => monitoraCadastro(idSolicitacao), 500); 
+                    
+                    contador++;
+                    
+                    if (contador == 240){ 
+
+                        const formData = new FormData();
+                        formData.append('id_solicitacao', idSolicitacao);
+                        formData.append('cracha', ''); 
+                        formData.append('flag', '1');
+
+                        try {
+
+                            const response = await fetch("../APIs/atualizaDB.php", {
+                            method: "POST",
+                            body: formData
+                            });
+
+                            const result = await response.json();
+
+                            if (result.ok){
+                                alert("Cadastro cancelado por tempo esgotado.");
+                            } else {
+                                alert("Erro ao cancelar o cadastro por tempo esgotado.");
+                            }
+
+                        } catch (err) {
+                            console.error("Erro ao enviar flag de timeout:", err);
+                        }
+                        
+                        contador = 0;
+                        flagCadastro = 0;
+                        window.location.href = "cadastro.php";
+
+                    } else {
+                        setTimeout(() => monitoraCadastro(idSolicitacao), 500);
+                    }
+
                 } else if (data.status_cadastro == 'erro') {
                     alert("Houve um erro no cadastro. Por favor, tente novamente.");
                     window.location.href = "cadastro.php";
+                    
                     flagCadastro = 0;
+
                 }
             }
         } catch (err) {
